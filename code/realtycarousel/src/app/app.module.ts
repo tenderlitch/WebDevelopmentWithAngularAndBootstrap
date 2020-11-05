9e5ab07afad16511129109fccd8f0beea47475d9
unknown <37c_xingzai@sina.com>
2020-11-05 10:52:01 +0800

    create new Component for realtycarousel

--- code/realtycarousel/src/app/app.module.ts
+++ code/realtycarousel/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { ListingsComponent } from './listings/listings.component';
+import { CreateListingComponent } from './create-listing/create-listing.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
+import { PreviewListingComponent } from './preview-listing/preview-listing.component';
+import { PhotosComponent } from './photos/photos.component';
+import { UploadPhotoComponent } from './upload-photo/upload-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    ListingsComponent,
+    CreateListingComponent,
+    EditListingComponent,
+    PreviewListingComponent,
+    PhotosComponent,
+    UploadPhotoComponent
   ],
   imports: [
     BrowserModule,
---
+++ code/realtycarousel/src/app/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sineup',
+  templateUrl: './sineup.component.html',
+  styleUrls: ['./sineup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
